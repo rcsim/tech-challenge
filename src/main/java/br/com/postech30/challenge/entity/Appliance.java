f7993cb8daadf4a7e493c48bbad896398fafb57d
Geovane Silveira <geovanedsilveira@gmail.com>
2023-08-22 18:13:52 -0300

    used annotation for lombok

--- src/main/java/br/com/postech30/challenge/entity/Appliance.java
+++ src/main/java/br/com/postech30/challenge/entity/Appliance.java
@@ -1,47 +1,35 @@
 package br.com.postech30.challenge.entity;
 
 import lombok.*;
-
 import jakarta.persistence.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "tb_appliance")
 @EqualsAndHashCode
 public class Appliance {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
     private Long id;
-    @Getter
-    @Setter
     private String name;
-    @Getter
-    @Setter
     private String model;
-    @Getter
-    @Setter
     private String power;
-    @Getter
-    @Setter
     private String manufacturer;
 
-    @Getter
-    @Setter
     @ManyToOne
     @JoinColumn(name = "address_id")
     private Address address;
 
-    @Getter
-    @Setter
     private Long userId;
 
     @ManyToMany(mappedBy = "applianceSet", cascade = CascadeType.ALL)
     private Set<Dependent> dependentSet =  new HashSet<>();
-
 
 }
