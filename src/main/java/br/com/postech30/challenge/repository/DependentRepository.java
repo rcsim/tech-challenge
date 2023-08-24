c3f821979da600f651893b8f500f377ecbfc6d60
Victor <victorptoledo@outlook.com>
2023-08-24 11:56:47 -0300

    Adicionando a pesquisa de dependentes cadastrados em um endereço e realizando algumas melhorias no código

--- src/main/java/br/com/postech30/challenge/repository/DependentRepository.java
+++ src/main/java/br/com/postech30/challenge/repository/DependentRepository.java
@@ -4,8 +4,6 @@ import br.com.postech30.challenge.entity.Dependent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -13,4 +11,5 @@ public interface DependentRepository extends JpaRepository<Dependent, Long> {
 
     List<Dependent> findByNameIgnoreCaseContainingOrDateOfBirthIgnoreCaseContainingOrGenderIgnoreCaseContainingOrParentageIgnoreCaseContaining(String name, String dateOfBirth, String gender, String parentage);
 
+    List<Dependent> findByAddress_Id(Long id);
 }
--- src/main/java/br/com/postech30/challenge/service/AddressService.java
+++ src/main/java/br/com/postech30/challenge/service/AddressService.java
@@ -1,6 +1,7 @@
 package br.com.postech30.challenge.service;
 
 import br.com.postech30.challenge.dto.AddressDTO;
+import br.com.postech30.challenge.dto.DependentDTO;
 
 import java.util.List;
 
@@ -15,4 +16,6 @@ public interface AddressService {
     void update(Long id, AddressDTO addressDTO);
 
     List<AddressDTO> search(String search);
+
+    List<DependentDTO> findByAddressId(Long id);
 }
