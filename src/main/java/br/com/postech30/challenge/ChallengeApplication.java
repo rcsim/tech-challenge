5bc21b5e941a2b5a16abcb04a097622738395110
Marcos Fernandes Alves <marcosfernandesalves99@hotmail.com>
2023-08-26 15:06:22 -0300

    implementando swagger

--- src/main/java/br/com/postech30/challenge/ChallengeApplication.java
+++ src/main/java/br/com/postech30/challenge/ChallengeApplication.java
@@ -2,12 +2,6 @@ package br.com.postech30.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 
